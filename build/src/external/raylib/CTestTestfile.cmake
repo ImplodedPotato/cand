469ac5c977acfe6a5bdf7133376484abfadd2178
James Miller <cask.zingy0o@icloud.com>
2025-11-09 09:09:21 -0800

    Delete build directory

--- build/src/external/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jamesmiller/dev/cand/src/external/raylib
-# Build directory: /Users/jamesmiller/dev/cand/build/src/external/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
--- build/src/external/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jamesmiller/dev/cand/src/external/raylib/src
-# Build directory: /Users/jamesmiller/dev/cand/build/src/external/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
